8e49257c49d4803f0e135be91ee6b62d212d9c63
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-11-16 19:12:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+  var firstInput = prompt("Enter the first number:");
+    var result = parseInt(firstInput) * 2;
+
+    // Take input for the second variable
+    var secondInput = prompt("Enter the second number:");
+
+    result += parseInt(secondInput);
+
+    // Display the final result using alert
+    alert("The final result is: " + result);
